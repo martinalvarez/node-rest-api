04ad3d88652085b7f0635de70dd4057c304bdd1a
Martin Alvarez <martinalvarez@gmail.com>
2023-08-22 14:44:46 -0300

    Add JSON data and endpoints

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
+const fs = require('node:fs');
+const fsp = require('node:fs/promises');
 const http = require('node:http');
 
-const server = http.createServer((req, res)=>{
-    const { 
-        headers,
-        method, 
-        url 
-    } = req;
+const responseHeaders = {
+    'json': {
+        'Content-Type': 'application/json',
+    },
+}
 
+function debugRequest({ headers, method, url }) {
     console.log(`*** ${method}: ${url} ***`);
 
     for (const key in headers) {
         const value = headers[key];
         console.log(`Key: ${key}: : ${value}`);
     }
+}
 
-    res.end('coming soon...');
+function sendResponse({ contentType = 'json', payload, res, statusCode=200 }) {
+    const [key, value ] = Object.entries(responseHeaders[contentType])[0];
+
+    res.setHeader(key, value);
+    res.statusCode = statusCode;    
+    res.end(payload);
+}
+
+const server = http.createServer((req, res)=>{
+    const { 
+        method, 
+        url 
+    } = req;
+
+    switch (method) {
+        case 'GET': {
+            switch(url) {
+                case '/albums': {
+                    fs.readFile('./data/albums.json', { encoding: 'utf-8'}, (error, data) => {
+                        if (error) {
+                            sendResponse({ payload: JSON.stringify({ error: 'error loading albums' }), res, statusCode: 500 });
+                        } else {
+                            sendResponse({ payload: data, res });
+                        }
+                    });
+                    break;
+                }
+                case '/biography': {
+                    fsp.readFile('./data/biography.json', { encoding: 'utf-8' })
+                        .then((file)=> {
+                            sendResponse({ payload: file, res, statusCode: 500 });
+                        })
+                        .catch((error)=> {
+                            sendResponse({ payload: JSON.stringify({ error: 'error loading biography' }), res, statusCode: 500 });
+                        });
+                    break;
+                }
+                case '/cerati': {
+                    sendResponse({ payload: JSON.stringify({ error: 'resource not found' }), res, statusCode: 404 });
+                    break;
+                }
+                default: {
+                    sendResponse({ payload: JSON.stringify({ message: 'coming soon' }), res});
+                    break;
+                }
+            }
+            break;  
+        }
+        default: {
+            sendResponse({ payload: { message: 'coming soon' }, res});
+            break;            
+        }
+    }
 });
 
 server.listen(3000, ()=>{
